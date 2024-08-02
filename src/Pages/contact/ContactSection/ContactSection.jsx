22a0b9637b0e176c7810995e14b75b390d0d3113
paramiwijesinghe <paramiwijesinghe05@gmail.com>
2024-08-02 21:05:06 +0700

    create achievement and contact pages

--- src/Pages/contact/ContactSection/ContactSection.jsx
+++ src/Pages/contact/ContactSection/ContactSection.jsx
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useState }  from "react";
 import './contactSection.css';
 
 const ContactSection = () => {
 
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+        console.log(formData);
+      };
+
     return (
         <div className="contact">
-        <h1>Contact Section</h1>
+            <div className="contactForm_container"> 
+            <form onSubmit={handleSubmit}>
+                <div className="contactUs_inputMain">
+                    <label htmlFor="name" className="contactUs_label">Your Name</label>
+                    <input className="contactUs_input"
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className="contactUs_inputMain">
+                    <label htmlFor="email" className="contactUs_label">Your Mail</label>
+                    <input className="contactUs_input"
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className="contactUs_inputMain">
+                    <label htmlFor="subject" className="contactUs_label">Subject</label>
+                    <input className="contactUs_input"
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    value={formData.subject}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className="contactUs_inputMain">
+                    <label htmlFor="message" className="contactUs_label">Your Message</label>
+                    <textarea className="contactUs_input"
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className="sendButton_container">
+                    <button type="submit" className="send_button">Send</button>
+                </div>
+                
+            </form>
+            </div>
+            
         </div>
     );
     };
--- src/Pages/feed/achivement/AchivementSection/AchivementSections.jsx
+++ src/Pages/feed/achivement/AchivementSection/AchivementSections.jsx
@@ -5,7 +5,40 @@ const AchivementSections = () => {
     
         return (
             <div className="achivement">
-            <h1>Achivement Section</h1>
+            <div className="awards_mainContainer">
+            <h1 className="awards_text">Awards</h1>
+                <div className="awards_container">
+                    <div className="awards_container2">
+                        <div className="awardsPhotos_container">
+                            <h3>Photos</h3>
+                        </div>
+                        <h3 className="awardTitle_text">Award title</h3>
+                        <div className="description_container">
+                            <h3 className="description_text">Description</h3>
+                        </div>
+                    </div>
+
+                    <div className="awards_container2">
+                        <div className="awardsPhotos_container">
+                            <h3>Photos</h3>
+                        </div>
+                        <h3 className="awardTitle_text">Award title</h3>
+                        <div className="description_container">
+                            <h3 className="description_text">Description</h3>
+                        </div>
+                    </div>
+
+
+                    
+                </div>
+
+            </div>
+            <div className="grants_mainContainer"></div>
+            <h1 className="awards_text">Grants & Funds</h1>
+            <div className="grants_container1">
+                <div className="grants_container2"></div>
+                <div className="grants_container2"></div>
+            </div>
             </div>
         );
         };
